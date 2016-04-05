afa89f3a8b307ef834fcdf3f13fadf1325774f42
walter lopez <wlopez@lapwlopezw7.domino.local>
2016-04-05 16:56:35 -0600

    Cambios en 05042016

--- App_Hospital/App_Hospital/Models/Especialidad.cs
+++ App_Hospital/App_Hospital/Models/Especialidad.cs
@@ -7,12 +7,15 @@ using System.ComponentModel.DataAnnotations;
 namespace App_Hospital.Models
 {
     public class Especialidad
-    {        
+    {
+        [ScaffoldColumn(true)]
+        public string Editar { get; set; }
         public int ID { get; set; }
         [Required(ErrorMessage="Ingrese nombre.")]
         [StringLength(25)]
         public string Nombre { get; set; }
         [StringLength(50)]
+        [Display(Name="Descripción")]
         public string Descripcion { get; set; }
     }
 }
---
+++ App_Hospital/App_Hospital/Models/EstadoCivil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App_Hospital.Models
+{
+    public class EstadoCivil
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int ID;
+
+        [Display(Name="Nombre")]
+        [StringLength(25)]
+        [Required(ErrorMessage="Ingrese nombre.")]
+        public string Nombre;
+
+        [Display(Name="Habilitado")]
+        public bool Habilitado;
+    }
+}
